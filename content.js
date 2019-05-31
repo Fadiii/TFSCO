e3d4589f9e8fea16c257ff66bc19128d04ead029
Fedil Al-Hayawi <fedil@hotmail.de>
2019-05-31 12:45:16 +0100

    Set font properly

--- content.js
+++ content.js
@@ -325,9 +325,8 @@ function rangeOnChangeAlpha(element){
 
 function rangeOnChangefontSize(element){
     $('#CS_fontSize').val(settings.currentFontSize);
-    element.style.setProperty('font-size', settings.currentFontSize +'px', 'important');
     // Add stroke
-    element.style.cssText = 'color: white; text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000';
+    element.style.cssText = 'font-size:'+settings.currentFontSize +'px;'+' color: white; text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000';
 
     $(document).on('input change', '#CS_fontSize', function() {
         settings.currentFontSize = this.value;
